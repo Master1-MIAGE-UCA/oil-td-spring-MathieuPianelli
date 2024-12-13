74780ba7547cf7a49e3f1eb399388050f6ceaad2
MathieuPianelli <mathieu.pianelli>
2024-12-13 10:03:10 +0100

    add swagger

--- src/main/java/unica/M1/Miage/SwaggerConfig.java
+++ src/main/java/unica/M1/Miage/SwaggerConfig.java
@@ -1,22 +1,20 @@
-//package unica.M1.Miage;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("votre.nom.de.paquet"))
-//                .build();
-//    }
-//}
+package unica.M1.Miage;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Dice Roll API")
+                        .version("1.0")
+                        .description("API for rolling dice and logging the results"));
+    }
+}
 
